378a91d085b65c2036a1d511297648c3e3daa603
TheRogueNinja <shreyassawant018@gmail.com>
2017-10-06 02:43:04 +0530

    Object Moving to mouse pos,needs rectifying

---
+++ Assets/Direct.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direct : MonoBehaviour {
+
+	public GameObject Arrow;
+	private Transform currentLoc = null, directLoc = null;
+	// Use this for initialization
+	void Start () {
+		//currentLoc.position = Arrow.transform.position;
+		Debug.Log(Arrow.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//directLoc.position = Input.mousePosition;
+		Debug.Log(Input.mousePosition);
+		transform.rotation = Quaternion.FromToRotation(Arrow.transform.position,Input.mousePosition);
+	}
+}
---
+++ Assets/WebCamText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamText : MonoBehaviour {
+
+	public GameObject webCamPlane;
+	// Use this for initialization
+	void Start () {
+		if(Application.isMobilePlatform){
+			GameObject camParent = new GameObject("camParent");
+			camParent.transform.position = this.transform.position;
+			this.transform.parent = camParent.transform;
+		}
+		Input.gyro.enabled = true;
+		WebCamTexture webCamT = new WebCamTexture();
+		webCamPlane.GetComponent<MeshRenderer>().material.mainTexture = webCamT;
+		webCamT.Play();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion camRot = new Quaternion(Input.gyro.attitude.x,Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+		this.transform.localRotation = camRot;
+	}
+}
